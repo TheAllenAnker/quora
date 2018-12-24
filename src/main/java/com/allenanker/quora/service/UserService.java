510a5826e50d64793c26d6b2cf32bb83eb7d11f7
TheAllenAnker <theallenanker@gmail.com>
2018-12-24 21:36:41 +0800

    Add logout function.

--- src/main/java/com/allenanker/quora/service/UserService.java
+++ src/main/java/com/allenanker/quora/service/UserService.java
@@ -24,6 +24,10 @@ public class UserService {
     @Autowired
     private LoginTicketDAO loginTicketDAO;
 
+    public void logout(String ticket) {
+        loginTicketDAO.updateStatus(ticket, 1);
+    }
+
     public Map<String, String> login(String username, String password) {
         Map<String, String> msgMap = new HashMap<>();
         if (StringUtils.isBlank(username)) {
