7c5db70427cf7947c394b24857c67084266ed386
TheAllenAnker <theallenanker@gmail.com>
2019-01-23 11:10:38 +0800

    Add queue event handler and jedis utils.

--- src/main/java/com/allenanker/quora/async/EventProducer.java
+++ src/main/java/com/allenanker/quora/async/EventProducer.java
@@ -1,5 +1,25 @@
 package com.allenanker.quora.async;
 
-public class EventProducer {
+import com.alibaba.fastjson.JSONObject;
+import com.allenanker.quora.util.JedisAdapter;
+import com.allenanker.quora.util.RedisKeyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class EventProducer {
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    public boolean fireEvent(EventModel eventModel) {
+        try {
+            String json = JSONObject.toJSONString(eventModel);
+            String key = RedisKeyUtils.getEventQueueKey();
+            jedisAdapter.lpush(key, json);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
